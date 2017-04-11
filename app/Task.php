7c210423641fa389bcf2bfc923dac2057f0cd91d
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-11 22:43:15 +0200

    Eloquent, Relacions amb el model Task. Definim una relació amb el model User (belongsTo).

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['name'];
+
+    /**
+     * Definim una relació amb el model User.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
