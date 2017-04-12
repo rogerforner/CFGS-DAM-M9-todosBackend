0d693ea7213ddc3574911822029f9bae1e275c80
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-12 17:37:45 +0200

    Entenent els layouts. Preparant el layout en el que començar a maquetar el todosBackend (views i partials).

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,1 +1,17 @@
 @extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    Tasks
+@endsection
+
+@section('contentheader_title')
+    Tasks
+@endsection
+
+@section('contentheader_description')
+    Pràctica del todosBackend.
+@endsection
+
+@section('main-content')
+    Contingut de la pàgina.
+@endsection
