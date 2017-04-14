c42a07670caf6c45cc5534a43e60e7048e9e5ff2
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-14 16:04:55 +0200

    Repositories; Actualment duem a terme una consulta a la base de dades amb el controlador TaskController, per tal de mostrar el total de tasques d'un usuari en concret. Un controlador mai hauria de realitzar consultes, això ho hem de fer amb un Repositori. Creem el repositori TaskRepository, el qual disposarà dels mètodes necessàris per a dur a terme tantes consultes com siguin necessàries.

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Task;
+use App\User;
+
+class TaskRepository
+{
+    /**
+     * Obtenir la llista de totes les tasques d'un usuari en concret.
+     *
+     * @param User $user
+     * @return mixed
+     */
+    public function forUser(User $user)
+    {
+        //Consulta a la base de dades.
+        return Task::where('user_id', $user->id)
+            ->orderBy('created_at', 'asc') //Ordenar la llista de forma ascendent, segons la data de creació.
+            ->get(); //Obtenir tota la informació de la db.
+    }
+}
