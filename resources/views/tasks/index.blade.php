4b04c15e57199cf448c4b8837d62eca8e0a5dda7
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-16 00:33:13 +0200

    Views/tasks - Paginació; Afegim els links de pàginació en la vista.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -115,14 +115,8 @@
                         </tbody>
                     </table>
                 </div><!-- .box-body -->
-                <div class="box-footer clearfix">
-                    <ul class="pagination pagination-sm no-margin pull-right">
-                        <li><a href="#">«</a></li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">»</a></li>
-                    </ul>
+                <div class="box-footer clearfix text-right">
+                    {{ $tasks->links() }}
                 </div><!-- .box-footer -->
             </div><!-- .box -->
         </div><!-- .row -->
