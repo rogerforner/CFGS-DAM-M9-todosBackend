ff9da7b6e6bfddcfae65967ffb78a5670b176fb2
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-17 23:55:42 +0200

    Event; Quan hem generat els events i listeners amb la comanda del commit anterior, se'ns han creat dues carpetes, /Events i /Listeners. En aquest cas ens hem centrat en l'event NewUser.php, en aque volem que quan es creï un nou objecte User, es dugui a terme un esdeveniment.

--- app/Events/NewUser.php
+++ app/Events/NewUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
@@ -13,15 +14,16 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 class NewUser
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $user;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
