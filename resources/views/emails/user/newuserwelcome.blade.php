4f9d99b5421bc57bef9c7263b2c894c9ec4bb8b2
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-17 23:12:46 +0200

    Views (user/newuserwelcome.blade.php); Hem modificat la plantilla que serà emprada per donar contingut al correu que sigui enviat.

--- resources/views/emails/user/newuserwelcome.blade.php
+++ resources/views/emails/user/newuserwelcome.blade.php
@@ -1,12 +1,14 @@
 @component('mail::message')
-# Introduction
+# Benvingut/da a la App todosBackend.
 
-The body of your message.
+Hola {{ $user->name }},
+todosBackend es tracta d'una **activitat** de 2DAM, curs 2016-17, de l'institut de l'Ebre.
 
-@component('mail::button', ['url' => ''])
-Button Text
+@component('mail::button', ['url' => 'http://acacha.org/mediawiki/2DAM_2016-17/todos#.WPUu_9-YFqM'])
+Acacha: Alumnes
 @endcomponent
 
 Thanks,<br>
-{{ config('app.name') }}
+{{-- config('app.name') --}}
+Roger Forner Fabre
 @endcomponent
