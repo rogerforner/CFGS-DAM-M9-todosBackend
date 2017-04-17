78584e73578109729d909ca0aab6a5a0167bc2c7
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-18 00:38:41 +0200

    Views (emails/user/newuserwelcome); Hem afegit un nou component, emprat per mostrar el correu electrònic de l'usuari. Podría ser una dada interessant a mostrar a l'usuari.

--- resources/views/emails/user/newuserwelcome.blade.php
+++ resources/views/emails/user/newuserwelcome.blade.php
@@ -4,6 +4,10 @@
 Hola {{ $user->name }},<br>
 todosBackend es tracta d'una **activitat** de 2DAM, curs 2016-17, de l'institut de l'Ebre.
 
+@component('mail::panel')
+    El correu electrònic amb el que t'has registrat és: {{ $user->email }}
+@endcomponent
+
 @component('mail::button', ['url' => 'http://acacha.org/mediawiki/2DAM_2016-17/todos#.WPUu_9-YFqM'])
 Acacha: Alumnes
 @endcomponent
