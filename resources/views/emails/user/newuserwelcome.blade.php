c4aee0ca76f145cc398130b7978ee9ebe57852fd
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-19 01:53:48 +0200

    Mail and Password Reset; Fins ara només rebiem el correu a Mailtrap, aquest no arrivaba a la bustia de l'usuari (gmail, outlook, etc.). Ho hem arreglat, ara els usuaris poden rebre el correu a la bústia i, a més a més, resetejar el seu password si aquests no el recorden :) Ah! Paciència, de vegades triga una miqueta a que arribi.

--- resources/views/emails/user/newuserwelcome.blade.php
+++ resources/views/emails/user/newuserwelcome.blade.php
@@ -1,12 +1,8 @@
 @component('mail::message')
 # Benvingut/da a la App todosBackend.
 
-Hola {{ $user->name }},<br>
+Hola,<br>
 todosBackend es tracta d'una **activitat** de 2DAM, curs 2016-17, de l'institut de l'Ebre.
-
-@component('mail::panel')
-    El correu electrònic amb el que t'has registrat és: {{ $user->email }}
-@endcomponent
 
 @component('mail::button', ['url' => 'http://acacha.org/mediawiki/2DAM_2016-17/todos#.WPUu_9-YFqM'])
 Acacha: Alumnes
@@ -14,5 +10,4 @@ Acacha: Alumnes
 
 Molt atentament,<br>
 Roger Forner Fabre
-{{-- config('app.name') --}}
 @endcomponent
