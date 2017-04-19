8279c9b79a813eae5a857d3ecaa3d6aa7bff5c37
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-19 11:15:27 +0200

    Seeders; Connectem els seeders per poder ser cridats i generats.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //Simple seeders
+        $this->call(UserAdminTableSeeder::class);
+
+        //Factories
+        $this->call(UsersTableSeeder::class);
     }
 }
