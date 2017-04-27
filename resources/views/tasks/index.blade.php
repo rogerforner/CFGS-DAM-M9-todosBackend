2bceb689255fcb508cabe7db4efdce5012fb75a9
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-04-27 12:41:42 +0200

    Millora Vistes; Hem millorat el layout de totes les vistes. A més a més, hem afegit el chat.blade.php en un directori chat (sempre es pot donar el cas de necessitar crear més fitxers), convertint el chat.blade per index.blade i, clar, hem modificat la ruta de la vista en el Chat Controller.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -17,13 +17,17 @@
         <!-- Accions exitoses
              ================================================== -->
         @if (Session::has('success'))
-            <div class="alert alert-success alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h4><i class="fa fa-check-circle"></i> Success:</h4>
-                <ul>
-                    <li>{{ Session::get('success') }}</li>
-                </ul>
-            </div>
+            <div class="row">
+                <div class="col-md-9 col-md-offset-1">
+                    <div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                        <h4><i class="fa fa-check-circle"></i> Success:</h4>
+                        <ul>
+                            <li>{{ Session::get('success') }}</li>
+                        </ul>
+                    </div>
+                </div><!-- .col- -->
+            </div><!-- .row -->
         @endif
 
         <!-- Tractament d'errors
@@ -31,97 +35,103 @@
              ================================================== -->
         @if (count($errors) > 0)
             <div class="row">
-                <div class="alert alert-danger alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                    <h4><i class="fa fa-warning"></i> Error:</h4>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
+                <div class="col-md-9 col-md-offset-1">
+                    <div class="alert alert-danger alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                        <h4><i class="fa fa-warning"></i> Error:</h4>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div><!-- .col- -->
+            </div><!-- .row -->
         @endif
 
         <div class="row">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <i class="fa fa-plus"></i> <h3 class="box-title">Add tasks</h3>
-                </div>
-                <div class="box-body">
-                    <!-- Formulari: crear tasques
-                    ================================================== -->
-                    <form action="{{ route('tasks.store') }}" method="POST">
-                        {{-- srf_field() = <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-                        {{ csrf_field() }}
+            <div class="col-md-9 col-md-offset-1">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <i class="fa fa-plus"></i> <h3 class="box-title">Add tasks</h3>
+                    </div>
+                    <div class="box-body">
+                        <!-- Formulari: crear tasques
+                        ================================================== -->
+                        <form action="{{ route('tasks.store') }}" method="POST">
+                            {{-- srf_field() = <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
+                            {{ csrf_field() }}
 
-                        <div class="input-group">
-                            <!-- Input: name
-                            ================================================== -->
-                            <input type="text" name="name" placeholder="Task name" class="form-control">
-                            <!-- Botó: submit
-                            ================================================== -->
-                            <span class="input-group-btn">
-                              <button type="submit" class="btn btn-info btn-flat">Add task</button>
-                            </span>
-                        </div>
-                    </form>
-                </div><!-- .box-body -->
-            </div><!-- .box -->
+                            <div class="input-group">
+                                <!-- Input: name
+                                ================================================== -->
+                                <input type="text" name="name" placeholder="Task name" class="form-control">
+                                <!-- Botó: submit
+                                ================================================== -->
+                                <span class="input-group-btn">
+                                  <button type="submit" class="btn btn-info btn-flat">Add task</button>
+                                </span>
+                            </div>
+                        </form>
+                    </div><!-- .box-body -->
+                </div><!-- .box -->
+            </div><!-- .col -->
         </div><!-- .row -->
 
         <div class="row">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <i class="fa fa-list"></i> <h3 class="box-title">All tasks</h3>
-                </div>
-                <div class="box-body">
-                    <!-- Taula: organitzar tasques
-                    ================================================== -->
-                    <table class="table table-bordered">
-                        <tbody>
-                        <tr>
-                            <th class="task-id">#</th>
-                            <th class="task-name">Tasks</th>
-                            <th class="task-action">Actions</th>
-                        </tr>
-                        <!-- Tasca
+            <div class="col-md-9 col-md-offset-1">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <i class="fa fa-list"></i> <h3 class="box-title">All tasks</h3>
+                    </div>
+                    <div class="box-body">
+                        <!-- Taula: organitzar tasques
                         ================================================== -->
-                        @foreach($tasks as $index => $task)
+                        <table class="table table-bordered">
+                            <tbody>
                             <tr>
-                                <td class="task-id">{{ $index+1 }}</td>
-                                <td title="Created at {{ $task->created_at }}" class="task-name">{{ $task->name }}</td>
-                                <td class="task-action">
-                                    <!-- Editar tasca
-                                    ================================================== -->
-                                    <a href="{{ route('tasks.edit', [$task->id]) }}" title="Edit" class="btn btn-default">
-                                        <i class="fa fa-pencil"></i>
-                                    </a>
-                                    <!-- Esborrar tasca
-                                    ================================================== -->
-                                    <form action="{{ route('tasks.destroy', [$task->id]) }}" method='POST' style="display: inline;">
-                                        {{ csrf_field() }}
-                                        {{-- El formulari és de tipus POST, encara que aquest està responent a una
-                                        sol·licitud de tipus emprant una ruta. Els formularis d'HTML només admeten
-                                        les peticions POST i GET, per la qual cosa hem de fer servir el mètode
-                                        method_field('DELETE') per tal de poder colar una peticío Delete. Aquest
-                                        mètode generarà un camp ocult que sobreescriurà el verdader:
-                                        <input type="hidden" name="_method" value="DELETE"> --}}
-                                        {{ method_field('DELETE') }}
-
-                                        <button type="submit" title="Delete" class="btn btn-danger">
-                                            <i class="fa fa-trash-o"></i>
-                                        </button>
-                                    </form>
-                                </td>
+                                <th class="task-id">#</th>
+                                <th class="task-name">Tasks</th>
+                                <th class="task-action">Actions</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div><!-- .box-body -->
-                <div class="box-footer clearfix text-right">
-                    {{ $tasks->links() }}
-                </div><!-- .box-footer -->
+                            <!-- Tasca
+                            ================================================== -->
+                            @foreach($tasks as $index => $task)
+                                <tr>
+                                    <td class="task-id">{{ $index+1 }}</td>
+                                    <td title="Created at {{ $task->created_at }}" class="task-name">{{ $task->name }}</td>
+                                    <td class="task-action">
+                                        <!-- Editar tasca
+                                        ================================================== -->
+                                        <a href="{{ route('tasks.edit', [$task->id]) }}" title="Edit" class="btn btn-default">
+                                            <i class="fa fa-pencil"></i>
+                                        </a>
+                                        <!-- Esborrar tasca
+                                        ================================================== -->
+                                        <form action="{{ route('tasks.destroy', [$task->id]) }}" method='POST' style="display: inline;">
+                                            {{ csrf_field() }}
+                                            {{-- El formulari és de tipus POST, encara que aquest està responent a una
+                                            sol·licitud de tipus emprant una ruta. Els formularis d'HTML només admeten
+                                            les peticions POST i GET, per la qual cosa hem de fer servir el mètode
+                                            method_field('DELETE') per tal de poder colar una peticío Delete. Aquest
+                                            mètode generarà un camp ocult que sobreescriurà el verdader:
+                                            <input type="hidden" name="_method" value="DELETE"> --}}
+                                            {{ method_field('DELETE') }}
+
+                                            <button type="submit" title="Delete" class="btn btn-danger">
+                                                <i class="fa fa-trash-o"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div><!-- .box-body -->
+                    <div class="box-footer clearfix text-right">
+                        {{ $tasks->links() }}
+                    </div><!-- .box-footer -->
+                </div><!-- .col- -->
             </div><!-- .box -->
         </div><!-- .row -->
     </div><!-- .container -->
